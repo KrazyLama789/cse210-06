433217d2ca7cf3a3e118ae53e9b668c593501b23
Jenny <smilingjenny@gmail.com>
2022-03-28 19:59:16 -0600

    Started to implement a run away action for combat

--- quest/game/scripting/control_combat_action.py
+++ quest/game/scripting/control_combat_action.py
@@ -6,9 +6,15 @@ class ControlCombatAction(Action):
 
     def __init__(self, keyboard_service):
         self._keyboard_service = keyboard_service
+        self.execute()
         
     def execute(self, cast, script, callback):
         adventurer = cast.get_first_actor(ADVENTURER_GROUP)
+        
+       #This is for a "run away" action during combat. It is only partially working. 
+        if self._keyboard_service.is_key_down("4"):
+            callback.on_next(IN_PLAY)
+
         # Kosei, build combat controlls here.
         # if self._keyboard_service.is_key_down(): 
         #     adventurer.move_left()
