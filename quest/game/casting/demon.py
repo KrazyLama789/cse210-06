f277cfddac7b7ff125b1d889f4e2e574df949f62
Jenny <smilingjenny@gmail.com>
2022-04-01 17:14:59 -0600

    Changed Demon's level mechanics

--- quest/game/casting/demon.py
+++ quest/game/casting/demon.py
@@ -8,7 +8,7 @@ import random
 class Demon(Character):
     """Demon."""
     
-    def __init__(self, body, animation, debug = False):
+    def __init__(self, body, animation, adventurer_level, debug = False):
         """Constructs a demon.
         
         Args:
@@ -17,8 +17,8 @@ class Demon(Character):
             debug: If it is being debugged. 
         """
         super().__init__(body, animation, debug)
-        self._level = random.randint(1, 9) #If we make the level two-digits, we will need to fix spacing from side margin
-        self._max_hp = self._level * 5
+        self._level = adventurer_level + random.randint(0,3)
+        self._max_hp = self._level + 5
         self._current_hp = self._max_hp 
 
         print(f"level: {self._level}")
