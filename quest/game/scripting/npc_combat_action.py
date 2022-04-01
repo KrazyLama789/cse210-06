a76901df6a72ea298d0d8dfce91e07a8588e8496
Jenny <smilingjenny@gmail.com>
2022-04-01 17:17:21 -0600

    Added back scene change after demon's turn

--- quest/game/scripting/npc_combat_action.py
+++ quest/game/scripting/npc_combat_action.py
@@ -14,7 +14,10 @@ class NpcCombatAction(Action):
         demon = cast.get_first_actor(DEMON_GROUP)
     
         attacks = [demon.action_1(), demon.action_2(), demon.action_3()]
-        adventurer.lose_hp(random.choice(attacks)) 
+        choose_attack = random.choice(attacks)
+        adventurer.lose_hp(choose_attack) 
+        print(f"Demon's attack: {choose_attack}")
+        callback.on_next(ADVENTURER_COMBAT)
 
         # if adventurer.get_current_hp() <= 0:
         #     callback.on_next(GAME_OVER)
